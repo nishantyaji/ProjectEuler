a2b82d4b487b66bbe1781b58fa9e440b0dd48ee6
nishantyaji <nishant.yaji@gmail.com>
2018-10-27 14:07:23 +0530

    Final implementation of problem 65 in project euler

--- Problem65.py
+++ Problem65.py
@@ -1,8 +1,25 @@
 fractions = [2]
 
-for i in range(1,100)
-    if(i%3 == 2)
-        fractions[i] = (int)(i+1)/3
-    :   fractions[i] = 1
+for i in range(1,100,1):
+    if((i+1)%3 == 0):
+        fractions.append((int)(2*(i+1)/3))
+    else:
+        fractions.append(1)
 
-print(fractions)
+def calculate(index):
+    return recur(index-1, (fractions[index],1))
+
+def recur(index, tuple):
+    if(index == 0):
+        return(2*tuple[0] + tuple[1], tuple[0])
+    else:
+        return recur(index-1, (fractions[index]*tuple[0] + tuple[1], tuple[0]))
+
+def sumofdigits(number):
+    string = str(number)
+    sum = 0
+    for x in string:
+        sum += int(x)
+    return sum
+
+print(sumofdigits(calculate(99)[0]))
